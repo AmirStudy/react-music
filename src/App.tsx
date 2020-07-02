27d9419b2228e26808801bcdf4e48b3263614d0e
Tendai Mutunhire <nomadhideout@yandex.com>
2020-07-02 20:44:11 +0200

    add core music player functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,126 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const AudioContext =
+  window.AudioContext || window.webkitAudioContext
+
+const audioContext = new AudioContext()
+const audioElement = document.querySelector(
+  'audio'
+)
+const track = audioContext.createMediaElementSource(
+  audioElement
+)
+
+const gainNode = audioContext.createGain()
+
+track
+  .connect(gainNode)
+  .connect(audioContext.destination)
 
 function App() {
-  const AudioContext =
-    window.AudioContext ||
-    window.webkitAudioContext
+  const [
+    audioProperties,
+    setAudioProperties
+  ] = useState({
+    playerStatus: 'stopped',
+    volume: 1
+  })
 
-  const audioContext = new AudioContext()
+  const handlePlay = () => {
+    console.log(
+      `handle play, old playerStatus: ${
+        audioProperties.playerStatus
+      }`
+    )
+    audioContext.resume()
+
+    let newStatus = ''
+    if (
+      audioProperties.playerStatus === 'playing'
+    ) {
+      newStatus = 'stopped'
+      audioElement?.pause()
+    } else {
+      newStatus = 'playing'
+      audioElement?.play()
+    }
+
+    // const newStatus =
+    //   audioProperties.playerStatus === 'playing'
+    //     ? 'stopped'
+    //     : 'playing'
+
+    setAudioProperties({
+      playerStatus: newStatus,
+      volume: audioProperties.volume
+    })
+
+    // audioProperties.playerStatus === "playing"
+    // ? audioElement?.play()
+    // : audioElement?.pause()
+
+    console.log(
+      `new playerStatus is ${
+        newStatus
+      }`
+    )
+  }
+
+  const changeVolume = (e) => {
+    console.log(
+      `updating volume to ${e.target.value}`
+    )
+    const newVolume = e.target.value
+    gainNode.gain.value = newVolume
+  }
 
   return (
     <div className="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="row">
-            <div class="col-md-4" />
-            <div class="col-md-4">
-              <div>
-                <h2 className="display-4">
-                  React Music!
-                </h2>
-                <h4 className="display-5 text-muted">
-                  <em>Welcome to React Music</em>
-                </h4>
-                <audio src="record.mp3">
-                  Audio
-                </audio>
-              </div>
-              <div class="text-center">
-                <img
-                  src="turntable.jpg"
-                  className="rounded img-fluid mb-3"
-                  alt="Turntable"
-                />
-                <p>
-                  <div class="btn-group btn-group-lg">
-                    <button
-                      type="button"
-                      class="btn btn-primary"
-                    >
-                      Play/Pause
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-primary"
-                    >
-                      Volume Slider
-                    </button>
-                    <input
-                      type="range"
-                      id="volume"
-                      class="control-volume"
-                      min="0"
-                      max="2"
-                      value="1"
-                      list="gain-vals"
-                      step="0.01"
-                      data-action="volume"
-                    />
-                  </div>
-                </p>
-              </div>
+      <div className="row">
+        <div className="col-xs-3 col-md-4" />
+        <div className="col-xs-6 col-md-4">
+          <div>
+            <h2 className="display-3">
+              React Music!
+            </h2>
+            <h4 className="display-5 text-muted">
+              <em>Welcome to React Music</em>
+            </h4>
+            <br />
+          </div>
+          <div className="text-center">
+            <img
+              src="turntable.jpg"
+              className="rounded img-fluid mb-3"
+              alt="Turntable"
+            />
+            <br />
+          </div>
+          <div className="col-xs-12 text-center">
+            <div className="btn-group btn-group-lg">
+              <button
+                type="button"
+                className="btn btn-primary mr-2"
+                onClick={handlePlay}
+              >
+                Play/Pause
+              </button>
+              <input
+                type="range"
+                id="volume"
+                name="volume"
+                className="control-volume"
+                min="0"
+                max="2"
+                defaultValue="1"
+                list="gain-vals"
+                step="0.01"
+                onChange={changeVolume}
+              />
             </div>
-            <div class="col-md-4" />
           </div>
         </div>
+        <div className="col-xs-3 col-md-4" />
       </div>
     </div>
   )
